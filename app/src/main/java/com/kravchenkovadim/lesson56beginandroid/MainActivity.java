0f851162643ef53c9287499963119b8e892a5c26
Arhizmei <arhizmei@gmail.com>
2024-03-16 22:17:49 +0200

    1.2

--- app/src/main/java/com/kravchenkovadim/lesson56beginandroid/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/lesson56beginandroid/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         VideoView videoView = findViewById(R.id.videoView);
-
+        videoView.setVideoPath("android.resource://"+getPackageName()+ "/" + R.raw.demo);
+        videoView.start();
     }
 }
