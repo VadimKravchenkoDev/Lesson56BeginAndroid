382859eac6ba1fab8dc694b7cc432bc26529d897
Arhizmei <arhizmei@gmail.com>
2024-03-16 22:37:02 +0200

    1.4

--- app/src/main/java/com/kravchenkovadim/lesson56beginandroid/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/lesson56beginandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.kravchenkovadim.lesson56beginandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
@@ -14,12 +15,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         VideoView videoView = findViewById(R.id.videoView);
-        String packageName = "com.kravchenkovadim.lesson56beginandroid";
-        videoView.setVideoPath("android.resource://"+packageName+ "/" + R.raw.demo);
 
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
+//
+//        String pathVideo = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+//
+//        Uri videoUri = Uri.parse(pathVideo);
+        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+        Uri vidUri = Uri.parse(vidAddress);
+        videoView.setVideoURI(vidUri);
+
+//        MediaController mediaController = new MediaController(this);
+//
+//        mediaController.setAnchorView(videoView);
+//
+//        videoView.setMediaController(mediaController);
+
         videoView.start();
+
     }
 }
